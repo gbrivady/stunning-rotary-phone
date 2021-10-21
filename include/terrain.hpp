1f5218dd1698c13512cae9070cbfe3e8d0b11318
g.brivady <gregory.brivady@gmail.com>
2021-10-21 19:38:54 +0200

    modified terrain for forces

--- include/terrain.hpp
+++ include/terrain.hpp
@@ -1,6 +1,18 @@
 #ifndef TERRAIN_HPP
 #define TERRAIN_HPP
+#include <tuple>
 
-class terrain;
+using namespace std;
+
+class Terrain{
+private:
+    tuple<double, double> g; //gravity
+    double nu; //viscosity
+public:
+    Terrain(tuple<double, double> g, double nu);
+    tuple<double, tuple<double, double>> hitbox(double x, double y);
+    tuple<double, double> getG();
+    double getNu();
+};
 
 #endif
--- src/terrain.cpp
+++ src/terrain.cpp
@@ -1,1 +1,18 @@
 #include "terrain.hpp"
+
+Terrain::Terrain(tuple<double, double> g, double nu){
+    this->g = g;
+    this->nu = nu;
+}
+
+tuple<double, tuple<double, double>> Terrain::hitbox(double x, double y){
+    return make_tuple(y-500, make_tuple(0,-1));
+}
+
+tuple<double, double> Terrain::getG(){
+    return this->g;
+}
+
+double Terrain::getNu(){
+    return this->nu;
+}
