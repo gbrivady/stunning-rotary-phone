d24f92934db7437af6cb7ab88e0d46b2c223b504
g.brivady <gregory.brivady@gmail.com>
2021-10-21 19:39:06 +0200

    modified entities for phX

--- include/entity.hpp
+++ include/entity.hpp
@@ -1,9 +1,13 @@
 #ifndef ENTITY_HPP
 #define ENTITY_HPP
 #include <tuple>
+#include <limits>
+#include "terrain.hpp"
 #include "physic/cinematic.hpp"
+#include "physic/hitbox.hpp"
 
-typedef std::tuple<float, float> Hitbox;
+using namespace std;
+
 typedef std::tuple<int, std::tuple<int, int, int>> Sprite;
 
 class Entity
@@ -12,8 +16,33 @@ private:
     Trajectory traj;
     Hitbox hitbox;
     Sprite sprite;
+    PhysicPar phxP;
+
 public:
-    Entity(Trajectory traj, Hitbox hitbox, Sprite sprite);
+    Entity(Trajectory traj, Hitbox hitbox, Sprite sprite, PhysicPar phxP);
+    Trajectory getTrajectory();
+    tuple<double, double> getSpeed();
+    tuple<double, double> getPosition();
+    Hitbox getHitbox();
+    Sprite getSprite();
+    PhysicPar getPhyX();
+    void setSpeed(tuple<double, double> v);
+    void setPosition(tuple<double, double> x);
+    void stopFall();
+    bool isFalling();
+    void move(tuple<double, double> u);
+    void accelerate(tuple<double, double> a);
+    void onCollision(Entity entity, tuple<double, tuple<double, double>> colVector);
+    void onCollision(Terrain terrain, tuple<double, tuple<double, double>> colVector);
+    
+};
+
+class PEntity: Entity{
+
+};
+
+class NPEntity: Entity{
+
 };
 
 
--- src/entity.cpp
+++ src/entity.cpp
@@ -1,7 +1,62 @@
 #include "entity.hpp"
 
-Entity::Entity(Trajectory traj, Hitbox hitbox, Sprite sprite){
+Entity::Entity(Trajectory traj, Hitbox hitbox, Sprite sprite, PhysicPar phxP){
     this->traj = traj;
     this->hitbox = hitbox;
     this->sprite = sprite;
+    this->phxP = phxP;
+}
+
+tuple<double, double> Entity::getSpeed(){
+    return this->traj.getSpeed();
+}
+
+tuple<double, double> Entity::getPosition(){
+    return this->traj.getPosition();
+}
+
+Hitbox Entity::getHitbox(){
+    return this->hitbox;
+}
+Sprite Entity::getSprite(){
+    return this->sprite;
+}
+PhysicPar Entity::getPhyX(){
+    return this->phxP;
+}
+
+void Entity::setPosition(tuple<double, double> x){
+    this->traj.setPosition(x);
+}
+
+void Entity::setSpeed(tuple<double, double> v){
+    this->traj.setSpeed(v);
+}
+
+void Entity::stopFall(){
+    this->setSpeed(make_tuple(get<0>(this->getSpeed()), 0));
+}
+bool Entity::isFalling(){
+    return get<1>(this->getSpeed()) != 0;
+}
+
+void Entity::move(tuple<double, double> u){
+    this->traj.move(u);
+}
+void Entity::accelerate(tuple<double, double> a){
+    this->traj.accelerate(a);
+}
+
+void Entity::onCollision(Entity, tuple<double, tuple<double, double>> colVector){
+
+}
+
+void Entity::onCollision(Terrain terrain, tuple<double, tuple<double, double>> colVector){
+    //gently moves it back to where it was
+    tuple<double, double> gowoBack = make_tuple(get<0>(colVector)*get<0>(get<1>(colVector))+1e-10,
+                                                get<0>(colVector)*get<1>(get<1>(colVector))+1e-10);
+    this->move(gowoBack);
+    this->stopFall();
+    // gowoBack = this->getSpeed();
+    // this->setSpeed(make_tuple(get<0>(gowoBack), -get<1>(gowoBack)));
 }
