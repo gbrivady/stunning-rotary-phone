e7b4b26f9bddae7e57479a6a3bc9c68dd30e6957
g.brivady <gregory.brivady@gmail.com>
2021-10-21 19:37:54 +0200

    updated cinematics for phX motor

--- include/physic/cinematic.hpp
+++ include/physic/cinematic.hpp
@@ -1,22 +1,38 @@
 #ifndef PHYSIC_CINEMATIC_HPP
 #define PHYSIC_CINEMATIC_HPP
 #include <tuple>
+#include <vector>
+
+using namespace std;
+
+class PhysicPar
+{
+private:
+    double mass;
+public:
+    PhysicPar() = default;
+    PhysicPar(double mass);
+    double getMass();
+};
 
 
 class Trajectory
 {
 private:
-    std::tuple<float, float> x;
-    std::tuple<float, float> v;
+    std::tuple<double, double> x;
+    std::tuple<double, double> v;
 
 public:
     Trajectory() = default;
-    Trajectory(std::tuple<float, float> x, std::tuple<float, float> v);
-    Trajectory(float x, float y, float vx, float vy);
-    std::tuple<float, float> getPosition();
-    std::tuple<float, float> getSpeed();
-    void setPosition(std::tuple<float, float> x);
-    void setSpeed(std::tuple<float, float> v);
+    Trajectory(std::tuple<double, double> x, std::tuple<double, double> v);
+    Trajectory(double x, double y, double vx, double vy);
+    std::tuple<double, double> getPosition();
+    std::tuple<double, double> getSpeed();
+    void setPosition(std::tuple<double, double> x);
+    void move(std::tuple<double, double> u);
+    void setSpeed(std::tuple<double, double> v);
+    void accelerate(tuple<double, double> a);
 };
+
 
 #endif
--- src/physic/cinematic.cpp
+++ src/physic/cinematic.cpp
@@ -1,27 +1,48 @@
 #include "physic/cinematic.hpp"
 
-Trajectory::Trajectory(std::tuple<float, float> x, std::tuple<float, float> v){
+PhysicPar::PhysicPar(double mass){
+    this->mass = mass;
+}
+
+double PhysicPar::getMass(){
+    return this->mass;
+}
+Trajectory::Trajectory(std::tuple<double, double> x, std::tuple<double, double> v){
     this->x = x;
     this->v = v;
 }
 
-Trajectory::Trajectory(float x, float y, float vx, float vy){
+Trajectory::Trajectory(double x, double y, double vx, double vy){
     this->x = std::make_tuple(x, y);
     this->v = std::make_tuple(vx, vy);
 }
 
-std::tuple<float, float> Trajectory::getPosition(){
+std::tuple<double, double> Trajectory::getPosition(){
     return this->x;
 }
 
-std::tuple<float, float> Trajectory::getSpeed(){
+std::tuple<double, double> Trajectory::getSpeed(){
     return this->v;
 }
 
-void Trajectory::setPosition(std::tuple<float, float> x){
+void Trajectory::setPosition(std::tuple<double, double> x){
     this->x = x;
 }
 
-void Trajectory::setSpeed(std::tuple<float, float> v){
-    this->x = x;
+void Trajectory::setSpeed(std::tuple<double, double> v){
+    this->v = v;
+}
+
+void Trajectory::move(tuple<double, double> u){
+    tuple<double, double> temp = this->x;
+    get<0>(temp) += get<0>(u);
+    get<1>(temp) += get<1>(u);
+    this->setPosition(temp);
+}
+
+void Trajectory::accelerate(tuple<double, double> a){
+    tuple<double, double> temp = this->v;
+    get<0>(temp) += get<0>(a);
+    get<1>(temp) += get<1>(a);
+    this->setSpeed(temp);
 }
